40bde9cecc718a0f7ce276e351ba6a21734c2574
amritasidhu <amritasidhu@live.com>
2022-09-16 21:51:11 -0700

    new

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # COSC499_A1
 Individual Github Excercise
 
-Branch 1 sorts 20 integers, ranging from 0 - 1067.
+Branch 1 sorts 10 integers, ranging from 0 - 1067.
--- sort.java
+++ sort.java
@@ -4,7 +4,7 @@ public class sort{
     public static void main(String [] args){
 
         //Branch 1 sorts 20 integers, ranging from 0 - 1067.
-        int array[] = {1067,7,0,17,50,8,4,119,5,202,1,34,6,9,79,2,3,23,29,12};
+        int array[] = {1067,6,9,79,2,3,23,29,12,389};
 
         Arrays.sort(array);
 
