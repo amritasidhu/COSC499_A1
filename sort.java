b71f832c3da7d185befc7a478e97c068f10de08e
amritasidhu <amritasidhu@live.com>
2022-09-16 20:51:32 -0700

    array

---
+++ sort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class sort{
+    public static void main(String [] args){
+        int array[] = {7,0,8,4,1,5,6,9,2,3};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
